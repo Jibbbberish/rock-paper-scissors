9d3b29c48002f8203c78a554bf1d01c2c1e5f653
Jake <jakethepiglet@gmail.com>
2024-07-12 19:30:10 -0500

    Add getHumanChoice function and make score variables

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randNum = Math.random();
     if (randNum < (1/3)) {
@@ -6,5 +9,17 @@ function getComputerChoice() {
         return "rock";
     } else {
         return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let initial = prompt("Choose Rock, Paper or Scissors!");
+    let choice = initial.toLowerCase();
+    
+    if (choice == "paper" || choice == "rock" || choice == "scissors") {
+        return choice;
+    } else {
+        alert("Choose rock, paper or scissors!");
+        getHumanChoice();
     }
 }

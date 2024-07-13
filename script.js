bc60cfdddac31a0200ca21b0f650826726b9d5c3
Jake <jakethepiglet@gmail.com>
2024-07-12 19:57:54 -0500

    Add playRound and Game functions and finish game

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ function getHumanChoice() {
         getHumanChoice();
     }
 }
+
+function playGame() {
+    function playRound(hChoice, cChoice) {
+        if (hChoice == "rock" && cChoice == "scissors") {
+            console.log("You win! Rock beats Scissors!");
+            humanScore += 1;
+        } else if (hChoice == "scissors" && cChoice == "paper") {
+            console.log("You win! Scissors beats Paper!");
+            humanScore += 1;
+        } else if (hChoice == "paper" && cChoice == "rock") {
+            console.log("You win! Paper beats Rock!");
+            humanScore += 1;
+        } else if (hChoice == "paper" && cChoice == "scissors") {
+            console.log("You lose! Scissors beats Paper!");
+            computerScore += 1;
+        } else if (hChoice == "scissors" && cChoice == "rock") {
+            console.log("You lose! Rock beats Scissors!");
+            computerScore += 1;
+        } else if (hChoice == "rock" && cChoice == "paper") {
+            console.log("You lose! Paper beats Rock!");
+            computerScore += 1;
+        } else {
+            console.log("You tie! You picked the same thing as the computer!");
+        }
+    }
+    
+    while (humanScore<3 && computerScore<3) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore >= 3) {
+        console.log("You win the game!")
+    } else if (computerScore >= 3) {
+        console.log("You lose the game. Better luck next time.")
+    }
+}
+
+playGame();
